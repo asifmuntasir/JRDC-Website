32934f0368e8798970791b34cd6d83304359bdc4
asifmuntasir <asif.neub1702@gmail.com>
2025-07-23 14:12:03 +0800

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div>This</div>
+
+      <div>is</div>
+
+      <div>Footer</div>
+    </>
+  );
+}
+
+export default Footer;
